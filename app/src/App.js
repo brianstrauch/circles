066c4c7c477b946d199d4c4f472724aa8db91588
Brian Strauch <bstrauch24@gmail.com>
2019-09-27 21:53:55 -0500

    Basic endpoint fetch

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
-function App() {
-  return <p>Hello World</p>;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      car: {
+        driver: '',
+        passengers: []
+      }
+    };
+  }
+
+  componentDidMount() {
+    get('http://localhost:5000/car').then(car => {
+      this.setState({car: car});
+    });
+  }
+
+  render() {
+    let { car } = this.state;
+
+    let passengers = car.passengers.map((passenger, idx) => {
+      return <li key={idx}>{passenger}</li>;
+    });
+
+    return (
+      <div>
+        <p>{car.driver}</p>
+        <ul>{passengers}</ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+function get(url) {
+  return fetch(url).then(res => res.json());
+}
--- server/server.py
+++ server/server.py
@@ -1,10 +1,19 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
-@app.route('/')
-def root():
-  return 'Hello World'
+@app.route('/car')
+def car():
+  car = {
+    'driver': 'Brian',
+    'passengers': ['Danny', 'Chelsea', 'Vishal']
+  }
+  return wrap_res(car)
+
+def wrap_res(res):
+  res = flask.jsonify(res)
+  res.headers.add('Access-Control-Allow-Origin', '*')
+  return res
 
 if __name__ == '__main__':
   app.run(debug=True)
