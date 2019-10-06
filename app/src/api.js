b1f9ba4af83c8a1c4314ecc0be325fa1ae5de89b
Brian Strauch <bstrauch24@gmail.com>
2019-10-06 11:35:23 -0500

    Basic styling

---
+++ app/src/api.js
@@ -0,0 +1,23 @@
+export function getPeople() {
+  return request('GET', '/person');
+}
+
+export function postPerson(person) {
+  return request('POST', '/person', person);
+}
+
+export function getCars() {
+  return request('GET', '/car');
+}
+
+export function postCar(car) {
+  return request('POST', '/car', car);
+}
+
+function request(method, endpoint, body) {
+  return fetch(endpoint, {
+    headers: {'Content-Type': 'application/json'},
+    method: method, 
+    body: JSON.stringify(body)
+  }).then(res => res.json());
+}
